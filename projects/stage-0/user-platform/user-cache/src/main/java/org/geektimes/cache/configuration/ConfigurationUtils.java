64a6b2fb1417438fc6e67f39e74dd4c2e8dc5c97
FuYi <thread_zhou@126.com>
2021-04-12 20:17:00 +0800

    feat: Add MutableEntryAdapter for user-cache module

--- projects/stage-0/user-platform/user-cache/src/main/java/org/geektimes/cache/configuration/ConfigurationUtils.java
+++ projects/stage-0/user-platform/user-cache/src/main/java/org/geektimes/cache/configuration/ConfigurationUtils.java
@@ -1,10 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.geektimes.cache.configuration;
 
-
-import javax.cache.configuration.CacheEntryListenerConfiguration;
-import javax.cache.configuration.CompleteConfiguration;
-import javax.cache.configuration.Configuration;
-import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
+import javax.cache.configuration.*;
 import javax.cache.event.CacheEntryEventFilter;
 import javax.cache.event.CacheEntryListener;
 
@@ -25,7 +37,7 @@ public abstract class ConfigurationUtils {
      * @return non-null
      * @see ImmutableCompleteConfiguration
      */
-    public static <K, V> CompleteConfiguration<K, V> completeConfiguration(Configuration<K, V> configuration) {
+    public static <K, V> CompleteConfiguration<K, V> immutableConfiguration(Configuration<K, V> configuration) {
         return new ImmutableCompleteConfiguration(configuration);
     }
 
--- projects/stage-0/user-platform/user-cache/src/main/java/org/geektimes/cache/redis/JedisCache.java
+++ projects/stage-0/user-platform/user-cache/src/main/java/org/geektimes/cache/redis/JedisCache.java
@@ -1,4 +1,5 @@
 package org.geektimes.cache.redis;
+
 import org.geektimes.cache.AbstractCache;
 import org.geektimes.cache.ExpireAbleEntry;
 import redis.clients.jedis.Jedis;
@@ -7,7 +8,7 @@ import javax.cache.CacheException;
 import javax.cache.CacheManager;
 import javax.cache.configuration.Configuration;
 import java.io.*;
-import java.util.Iterator;
+import java.util.Set;
 
 public class JedisCache<K extends Serializable, V extends Serializable> extends AbstractCache<K, V> {
 
@@ -37,9 +38,9 @@ public class JedisCache<K extends Serializable, V extends Serializable> extends 
     }
 
     @Override
-    protected void putEntry(ExpireAbleEntry<K, V> newEntry) throws CacheException, ClassCastException {
-        byte[] keyBytes = serialize(newEntry.getKey());
-        byte[] valueBytes = serialize(newEntry.getValue());
+    protected void putEntry(ExpireAbleEntry<K, V> entry) throws CacheException, ClassCastException {
+        byte[] keyBytes = serialize(entry.getKey());
+        byte[] valueBytes = serialize(entry.getValue());
         jedis.set(keyBytes, valueBytes);
     }
 
@@ -52,12 +53,14 @@ public class JedisCache<K extends Serializable, V extends Serializable> extends 
     }
 
     @Override
-    protected void doClear() throws CacheException {
+    protected void clearEntries() throws CacheException {
         // TODO
     }
 
+
     @Override
-    protected Iterator<Entry<K, V>> newIterator() {
+    protected Set<K> keySet() {
+        // TODO
         return null;
     }
 
