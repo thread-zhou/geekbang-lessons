84e3ed403ae7f2d48299270f184b4faf21038c51
FuYi <thread_zhou@126.com>
2021-04-12 20:23:22 +0800

    test: Add Test-case for user-cache module

--- projects/stage-0/user-platform/user-cache/src/main/java/org/geektimes/cache/configuration/ImmutableCompleteConfiguration.java
+++ projects/stage-0/user-platform/user-cache/src/main/java/org/geektimes/cache/configuration/ImmutableCompleteConfiguration.java
@@ -1,10 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.geektimes.cache.configuration;
-
 
 import javax.cache.configuration.*;
 import javax.cache.expiry.ExpiryPolicy;
 import javax.cache.integration.CacheLoader;
 import javax.cache.integration.CacheWriter;
+import java.util.Objects;
 
 /**
  * Immutable {@link CompleteConfiguration}
@@ -85,4 +101,16 @@ public class ImmutableCompleteConfiguration<K, V> implements CompleteConfigurati
         return configuration.isStoreByValue();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof Configuration)) return false;
+        Configuration<?, ?> that = (Configuration<?, ?>) o;
+        return Objects.equals(configuration, new ImmutableCompleteConfiguration(that).configuration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(configuration);
+    }
 }
